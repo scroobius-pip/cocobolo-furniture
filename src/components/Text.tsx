c79b4b3dd9c072c0c8cbef51362f16b25635f9d7
scroobius-pip <sim04ful@gmail.com>
2020-12-19 09:15:57 +0100

    Replaced Text Component h1 with more generic span

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -3,12 +3,17 @@ import React, { ReactNode, } from 'react'
 interface Props {
     children: ReactNode
     className?: string
-
 }
 
 
 export const Medium = ({ children, className }: Props) => {
-    return <h3 className={`text-xl  ${className}`}>
+    return <span className={`text-xl  ${className}`}>
         {children}
-    </h3>
+    </span>
+}
+
+export const Small = ({ children, className }: Props) => {
+    return <span className={`text-lg  ${className}`}>
+        {children}
+    </span>
 }
